845ea4519c6f4aeeb309c46b7dfb38b26a926895
sumi akther <iamsumiakther@gmail.coom>
2025-12-02 11:33:54 +0600

    AppHeader moved into QueryProvider

--- app/layout.js
+++ app/layout.js
@@ -14,8 +14,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <AppHeader />
         <QueryProvider>
+          <AppHeader />
           <ConfigProvider theme={theme}>{children}</ConfigProvider>
         </QueryProvider>
         <FooterSection />
